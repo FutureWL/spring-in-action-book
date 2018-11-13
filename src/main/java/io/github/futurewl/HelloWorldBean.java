7730ba1769e10ac5489e28482f0111b1789c8d03
魏来 <624263934@qq.com>
2018-11-13 14:03:50 +0800

    update

---
+++ src/main/java/io/github/futurewl/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package io.github.futurewl;
+
+/**
+ * 功能描述：程序清单 1.1
+ *
+ * @author weilai create by 2018/11/13:2:01 PM
+ * @version 1.0
+ */
+public class HelloWorldBean {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/io/github/futurewl/Main.java
@@ -0,0 +1,10 @@
+package io.github.futurewl;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2018/11/13:2:00 PM
+ * @version 1.0
+ */
+public class Main {
+}
