471629d3f4cd14ab665c5222a8bfadb30e0b36cf
魏来 <624263934@qq.com>
2018-11-26 16:44:43 +0800

    update

---
+++ part-1/chapter-1/src/main/java/io/github/futurewl/spring/chapter1/MainApplication.java
@@ -0,0 +1,13 @@
+package io.github.futurewl.spring.chapter1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：Chapter1
+ *
+ * @author weilai create by 2018/11/26:4:42 PM
+ * @version 1.0
+ */
+@SpringBootApplication
+public class MainApplication {
+}
