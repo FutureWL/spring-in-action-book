5664278cbb9369904a1e29223dc845282380fc8c
FutureWL <624263934@qq.com>
2019-04-08 17:35:45 +0800

    chapter1 1.9 1.10 1.11

--- Part-1/chapter-1/src/main/java/com/springinaction/knights/KnightMain.java
+++ Part-1/chapter-1/src/main/java/com/springinaction/knights/KnightMain.java
@@ -10,9 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class KnightMain {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext(
-                        "META-INF/spring/knight.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
